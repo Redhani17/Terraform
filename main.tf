1cc681a27570474a3fffeb4a1fae5306d7d7e545
Redhani17 <redhani1705@gmail.com>
2026-02-13 14:42:58 +0530

    Add security group and update EC2 instance configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,55 @@
-resource "aws_instance" "dev_server" {
-count=5
-  ami           = var.ami_id
-  instance_type = var.instance_type
+# -----------------------------
+# Security Group
+# -----------------------------
+resource "aws_security_group" "devops_sg" {
 
-  user_data = file("userdata.sh")
+  name        = "devops-sg"
+  description = "Allow SSH and Jenkins access"
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Jenkins Access"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "server"
+    Name = "DevOps-SG"
   }
 }
 
 
+# -----------------------------
+# EC2 Instance
+# -----------------------------
+resource "aws_instance" "dev_server" {
+
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  associate_public_ip_address = true
+
+  user_data = file("userdata.sh")
+
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "ALLSERVERS"
+  }
+}
