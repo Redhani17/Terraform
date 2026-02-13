3fe3bfbd5ce83b47fdd56c7e5dcc75ccbf0e1cdb
Redhani17 <redhani1705@gmail.com>
2026-02-13 15:13:36 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+# Update system
 yum update -y
 
+# -----------------------------
 # Install Git
+# -----------------------------
 yum install -y git
 
+# -----------------------------
 # Install Docker
+# -----------------------------
 yum install -y docker
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 
-# Install Java
+# -----------------------------
+# Install Java (Required for Jenkins)
+# -----------------------------
 amazon-linux-extras install java-openjdk11 -y
 
+# -----------------------------
 # Install Jenkins
+# -----------------------------
 wget -O /etc/yum.repos.d/jenkins.repo \
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
@@ -22,5 +31,6 @@ rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 yum install -y jenkins
 
+systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
