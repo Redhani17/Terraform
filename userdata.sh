2c449555fce6111e3c70055725a146c8c761da55
Redhani17 <redhani1705@gmail.com>
2026-02-13 14:43:39 +0530

    Refactor userdata.sh to install Git, Docker, Java, and Jenkins
    Updated the script to use dnf for package management and added installations for Git, Docker, Java, and Jenkins.

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y nginx
-sudo systemctl start nginx
+
+sudo dnf update -y
+
+# Install Git
+sudo dnf install git -y
+
+# Install Docker
+sudo dnf install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+
+# Install Java
+sudo dnf install java-17-amazon-corretto -y
+
+# Install Jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo dnf install jenkins -y
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
